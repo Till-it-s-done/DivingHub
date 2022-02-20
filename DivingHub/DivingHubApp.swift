23fc5babeb375e1666fbac5e297eb19eb3a24f60
Anupat Insuvanno <rickgrade3@gmail.com>
2022-02-20 09:35:21 +0700

    Initial Commit

---
+++ DivingHub/DivingHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  DivingHubApp.swift
+//  DivingHub
+//
+//  Created by Anupat Insuvanno on 20/2/2565 BE.
+//
+
+import SwiftUI
+
+@main
+struct DivingHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
